fe5219a2c1c897f45b7012db4907d2d57fc9d749
Emanuel Fernandes <efernandes@tektorque.com>
2019-10-30 14:14:51 +0000

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,8 @@
+// https://github.com/microsoft/vscode-extension-samples/blob/master/code-actions-sample/package.json
+// https://github.com/microsoft/vscode-extension-samples/tree/master/completions-sample
+
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+    
+}
